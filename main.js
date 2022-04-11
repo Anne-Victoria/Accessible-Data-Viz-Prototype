0d1710343089b555aefa3342e3e62ed11caadd8d
Anne-Victoria <2987773-Anne-Victoria@users.noreply.gitlab.com>
2022-04-11 16:44:41 +0100

    Fix path to csv file

--- main.js
+++ main.js
@@ -2,15 +2,17 @@
 console.log(d3);
 
 const fetchData = async () => {
-  const result = await d3.csv('metal_bands_2017.csv', (row, index) => ({
+  const result = await d3.csv('data/metal_bands_2017.csv', (row, index) => ({
     rank: index + 1,
     name: row.band_name,
     fans: row.fans,
     formed: row.formed,
     origin: row.origin,
   }));
-  console.log({ result });
-  return result;
+  let bandsByCounty = d3.group(result, (d) => d.origin);
+
+  console.log(bandsByCounty);
+  return bandsByCounty;
 };
 
 fetchData();
